4b2fa5a075d8865a83510a4782d2494e9d713647
Talkontar <artempopovartempopov@gmail.com>
2022-09-03 17:20:46 +0300

    changes

--- FirstProgram/FirstProgram/Program.cs
+++ FirstProgram/FirstProgram/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string str = "Hello, World!";
+            Console.WriteLine(str);
         }
     }
 }
